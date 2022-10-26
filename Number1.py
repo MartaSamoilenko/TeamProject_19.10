84161094ac4b24c13af9ad0ec73f41d5dae39da1
Аліна Бабенко <albabenko18@gmail.com>
2022-10-26 17:10:37 +0300

    total_occurences

---
+++ Number1.py
@@ -0,0 +1,21 @@
+def total_occurrences(s1, s2, ch):
+    """
+    (str, str, str) -> int
+    Precondition: len(ch) == 1
+    Return the total number of times that ch occurs in s1 and s2.
+    >>> total_occurrences('color', 'yellow', 'l')
+    3
+    >>> total_occurrences('red', 'blue', 'l')
+    1
+    >>> total_occurrences('green', 'purple', 'b')
+    0
+    """
+    s_all = s1+s2
+    if ch in s_all:
+        number_ch = (s_all).count(ch)
+        return number_ch
+    return 0
+
+# if __name__ == "__main__":
+#   import doctest
+#   print(doctest.testmod())
